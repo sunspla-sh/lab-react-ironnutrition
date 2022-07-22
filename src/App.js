e459085a773bc4af5b25c37676fc847222c83b86
jack <jrfisch95@gmail.com>
2022-07-22 17:43:29 -0400

    finish and commit for students

--- src/App.js
+++ src/App.js
@@ -1,23 +1,99 @@
 import logo from './logo.svg';
 import './App.css';
+import foods from './foods.json';
+
+import FoodBox from './components/FoodBox';
+
+import { Divider, Row } from 'antd';
+
+import { useState } from 'react';
+import AddFoodForm from './components/AddFoodForm';
+import Search from './components/Search';
 
 function App() {
+
+  const [foodForm, setFoodForm] = useState({
+    name: '',
+    calories: 0,
+    servings: 1,
+    image: "https://i.imgur.com/eTmWoAN.png"
+  });
+
+  const updateFoodForm = event => {
+    setFoodForm({
+      ...foodForm,
+      [event.currentTarget.name]: event.currentTarget.value
+    })
+  }
+
+  const handleAddFood = event => {
+    event.preventDefault();
+
+    const foodFormCopy = {...foodForm}
+
+    setFoodArray([
+      ...foodArray,
+      foodFormCopy
+    ])
+
+    setFoodForm({
+      name: '',
+      calories: 0,
+      servings: 1,
+      image: "https://i.imgur.com/eTmWoAN.png"
+    })
+
+  }
+
+  const [foodArray, setFoodArray] = useState(foods);
+  const [filteredFoodArray, setFilteredFoodArray] = useState(foods);
+
+  const [search, setSearch] = useState('');
+
+  const updateSearch = event => {
+
+    const newFilteredArray = foodArray.filter(individualFood => {
+      return individualFood.name.toLowerCase().includes(event.currentTarget.value.toLowerCase());
+    });
+
+    setFilteredFoodArray(newFilteredArray);
+
+    setSearch(event.currentTarget.value);
+  }
+
+  const handleDeleteFood = name => {
+    const foundFoodIndex = foodArray.findIndex(individualFood => individualFood.name === name);
+
+    const foodArrayCopy = [...foodArray];
+
+    const removedFood = foodArrayCopy.splice(foundFoodIndex, 1);
+
+    const newFilteredArray = foodArrayCopy.filter(individualFood => {
+      return individualFood.name.toLowerCase().includes(search.toLowerCase());
+    });
+
+    setFoodArray(foodArrayCopy);
+
+    setFilteredFoodArray(newFilteredArray);
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+      }}>
+        <AddFoodForm foodForm={foodForm} updateFoodForm={updateFoodForm} handleAddFood={handleAddFood} />
+        <Search search={search} updateSearch={updateSearch} />
+      </div>
+      <Divider>Food List</Divider>
+      <Row style={{ width: '100%', justifyContent: 'center' }}>
+        {filteredFoodArray.map(eachFood => {
+          return <FoodBox food={eachFood} handleDeleteFood={handleDeleteFood} />;
+        })}  
+      </Row>
     </div>
   );
 }
